095521dfa695097f5ecc9d351ca0e66144267c27
zhaoz0314 <92612715+zhaoz0314@users.noreply.github.com>
2023-10-03 15:42:43 -0400

    Add files via upload

--- pseudospatial/functions.py
+++ pseudospatial/functions.py
@@ -455,6 +455,18 @@ def multi_len_pr_tr_os_s_initializer(condition_n_s, window_len_s):
   return([jnp.zeros(tuple(condition_n_s) + (window_len_n + 1, ))
           for stat_idx in range(3)])
 
+# weighted covariances
+def weighted_cov_s_fct(traj_s, mean_s, weight_s):
+  # find shapes
+  frame_n = traj_s.shape[-1]
+  # compute
+  mean_s = jnp.expand_dims(mean_s, -1)
+  deviation_s = traj_s - mean_s
+  cov_s = (deviation_s @ jnp.swapaxes(deviation_s * weight_s, -1, -2)
+           / jnp.sum(weight_s))
+  return(cov_s)
+weighted_cov_s_fct = jax.jit(weighted_cov_s_fct)
+
 # pr tr os for nTs
 def multi_len_pr_tr_os_s_fct(traj, resolution,
                              window_len_s, samp_separation,
@@ -464,12 +476,18 @@ def multi_len_pr_tr_os_s_fct(traj, resolution,
   kernel_half_step_n = step_n // 4
   window_len_n = window_len_s.shape[0]
   samp_n = (step_n - 1) // samp_separation + 1
-  # half of the window as the kernel std, then normalize
-  kernel_s = jnp.exp(
-    -((jnp.arange(-kernel_half_step_n,
-                  kernel_half_step_n) / resolution) ** kernel_power
-      / (2 * jnp.expand_dims(window_len_s / 2, 1) ** kernel_power)))
-  kernel_s = kernel_s / jnp.sum(kernel_s, axis = -1, keepdims = True)
+  # create kernel, half of the window as width/std, then normalize
+  if kernel_power == "inf":
+    kernel_s = jnp.heaviside(jnp.arange(-kernel_half_step_n, kernel_half_step_n)
+                             + jnp.expand_dims(window_len_s / 2, 1) * resolution, 0)
+    kernel_s = kernel_s * kernel_s[::-1]
+    kernel_s = kernel_s / jnp.sum(kernel_s, axis = -1, keepdims = True)
+  else:
+    kernel_s = jnp.exp(
+      -((jnp.arange(-kernel_half_step_n,
+                    kernel_half_step_n) / resolution) ** kernel_power
+        / (2 * jnp.expand_dims(window_len_s / 2, 1) ** kernel_power)))
+    kernel_s = kernel_s / jnp.sum(kernel_s, axis = -1, keepdims = True)
   # find reference values (window at full length)
   full_mean = mean_s_fct(traj)
   full_cov = cov_s_fct(traj, full_mean)
@@ -485,17 +503,18 @@ def multi_len_pr_tr_os_s_fct(traj, resolution,
     jnp.zeros((window_len_n + 1, )).at[-1].set(full_tr),
     jnp.zeros((window_len_n + 1, )).at[-1].set(1)]
   def single_len_pr_tr_os_updater(window_len_idx, multi_len_pr_tr_os_s):
-    temp_windowed_fluct_s = jax.lax.fori_loop(
+    temp_segmented_fluct_s = jax.lax.fori_loop(
       0, samp_n,
       lambda samp_idx, windowed_fluct_s:
       windowed_fluct_s.at[
         samp_idx].set(
           jax.lax.dynamic_slice(traj,
                                 (0, window_len_idx * samp_separation),
-                                (part_n, kernel_half_step_n * 2))
-          * kernel_s[window_len_idx]),
+                                (part_n, kernel_half_step_n * 2))),
       jnp.zeros((samp_n, part_n, kernel_half_step_n * 2)))
-    temp_cov_s = cov_s_fct(temp_windowed_fluct_s, jnp.zeros((part_n, )))
+    temp_cov_s = weighted_cov_s_fct(temp_segmented_fluct_s,
+                                    jnp.zeros((part_n, )),
+                                    kernel_s[window_len_idx])
     temp_pc_s = es_s_fct(temp_cov_s)
     temp_pr = jnp.mean(dim_r_s_fct(temp_pc_s[0]))
     temp_tr_s = size_s_fct(temp_pc_s[0])
@@ -511,7 +530,7 @@ def multi_len_pr_tr_os_s_fct(traj, resolution,
                                            single_len_pr_tr_os_updater,
                                            multi_len_pr_tr_os_s)
   return(multi_len_pr_tr_os_s)
-multi_len_pr_tr_os_s_fct = jax.jit(multi_len_pr_tr_os_s_fct, static_argnums = (3, ))
+multi_len_pr_tr_os_s_fct = jax.jit(multi_len_pr_tr_os_s_fct, static_argnums = (3, 4))
 
 # ## pr tr os for nTs
 # #def multi_len_pr_tr_os_s_fct(traj, resolution,
@@ -521,12 +540,18 @@ multi_len_pr_tr_os_s_fct = jax.jit(multi_len_pr_tr_os_s_fct, static_argnums = (3
 # #  step_n = traj.shape[-1]
 # #  kernel_half_step_n = step_n // 4
 # #  window_len_n = window_len_s.shape[0]
-# #  # half of the window as the kernel std, then normalize
-# #  kernel_s = jnp.exp(
-# #    -((jnp.arange(-kernel_half_step_n,
-# #                  kernel_half_step_n) / resolution) ** kernel_power
-# #      / (2 * jnp.expand_dims(window_len_s / 2, 1) ** kernel_power)))
-# #  kernel_s = kernel_s / jnp.sum(kernel_s, axis = -1, keepdims = True)
+# #  # create kernel, half of the window as width/std, then normalize
+# #  if kernel_power == "inf":
+# #    kernel_s = jnp.heaviside(jnp.arange(-kernel_half_step_n, kernel_half_step_n)
+# #                             + jnp.expand_dims(window_len_s / 2, 1) * resolution, 0)
+# #    kernel_s = kernel_s * kernel_s[::-1]
+# #    kernel_s = kernel_s / jnp.sum(kernel_s, axis = -1, keepdims = True)
+# #  else:
+# #    kernel_s = jnp.exp(
+# #      -((jnp.arange(-kernel_half_step_n,
+# #                    kernel_half_step_n) / resolution) ** kernel_power
+# #        / (2 * jnp.expand_dims(window_len_s / 2, 1) ** kernel_power)))
+# #    kernel_s = kernel_s / jnp.sum(kernel_s, axis = -1, keepdims = True)
 # #  # find reference values (window at full length)
 # #  full_mean = mean_s_fct(traj)
 # #  full_cov = cov_s_fct(traj, full_mean)
@@ -565,7 +590,7 @@ multi_len_pr_tr_os_s_fct = jax.jit(multi_len_pr_tr_os_s_fct, static_argnums = (3
 # #                                           single_len_pr_tr_os_updater,
 # #                                           multi_len_pr_tr_os_s)
 # #  return(multi_len_pr_tr_os_s)
-# #multi_len_pr_tr_os_s_fct = jax.jit(multi_len_pr_tr_os_s_fct, static_argnums = (3, ))
+# #multi_len_pr_tr_os_s_fct = jax.jit(multi_len_pr_tr_os_s_fct, static_argnums = (3, 4))
 
 
 
